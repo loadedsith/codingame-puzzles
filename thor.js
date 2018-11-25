9932ba5b692034c4edb0508b0afcefdb872e3eb0
Graham Heath <heathg@google.com>
2018-11-25 16:03:30 -0600

    added complete thor

--- thor.js
+++ thor.js
@@ -10,20 +10,37 @@ const lightX = parseInt(inputs[0]); // the X position of the light of power
 const lightY = parseInt(inputs[1]); // the Y position of the light of power
 const initialTX = parseInt(inputs[2]); // Thor's starting X position
 const initialTY = parseInt(inputs[3]); // Thor's starting Y position
-let currentTX = parseInt(inputs[2]);
-let currentTY = parseInt(inputs[3]);
+let currentThorX = parseInt(inputs[2]);
+let currentThorY = parseInt(inputs[3]);
 let deltaX = 0;
 let deltaY = 0;
 
 // game loop
 while (true) {
     const remainingTurns = parseInt(readline()); // The remaining amount of turns Thor can move. Do not remove this line.
-
+    let direction = '';
     // Write an action using print()
-    switch ([deltaX, deltaY]) {
+    if (lightY < currentThorY) {
+      direction = direction + 'N';
+      currentThorY--;
+    }
+    if (lightY > currentThorY) {
+      direction = direction + 'S';
+      currentThorY++;
+    }
+    if (lightX < currentThorX) {
+      direction = direction + 'W';
+      currentThorX--;
+    }
+    if (lightX > currentThorX) {
+      direction = direction + 'E';
+      currentThorX++;
+    }
+
     // To debug: printErr('Debug messages...');
 
 
+
     // A single line providing the move to be made: N NE E SE S SW W or NW
-    print('SE');
+    print(direction);
 }
